555dc0f55282196f763adf8172b184513df73362
Kunal Kishore <kunalkishore1807@gmail.com>
2023-01-08 14:15:08 +0530

    app.js and theme.js updated

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,6 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
-import { fontSize } from "@mui/system";
+
 
 //color design tokens
 
@@ -163,8 +163,54 @@ export const themeSettings = (mode) => {
           }),
     },
     typography:{
-        fontFamily:['Source Sans Pro', 'sans serif'].join(",")
-        fontSize
+        fontFamily:['Source Sans Pro', 'sans serif'].join(","),
+        fontSize: 12,
+        h1:{
+          fontFamily:['Source Sans Pro','sans serif'].join(","),
+          fontSize: 40,
+        },
+        h2:{
+          fontFamily:['Source Sans Pro','sans serif'].join(","),
+          fontSize: 32,
+        },
+        h3:{
+          fontFamily:['Source Sans Pro','sans serif'].join(","),
+          fontSize: 24,
+        },
+        h4:{
+          fontFamily:['Source Sans Pro','sans serif'].join(","),
+          fontSize: 20,
+        },
+        h5:{
+          fontFamily:['Source Sans Pro','sans serif'].join(","),
+          fontSize: 16
+        },
+        h6:{
+          fontFamily:['Source Sans Pro','sans serif'].join(","),
+          fontSize: 14,
+        },
     }
   };
 };
+
+// context for color mode
+
+export const ColorModeContext = createContext({
+  toggleColorMode: ()=>{}
+});
+
+export const useMode = () =>{
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+      setMode((prev) => (prev === 'light' ? 'dark' : 'light')),
+    }),
+    []
+  );
+
+  const theme = useMode(()=> createTheme(themeSettings(mode)),[mode]);
+
+  return [theme, colorMode];
+}
