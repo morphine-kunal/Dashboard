c80212dfc31940b35cf011175c1978adf4839a65
Kunal Kishore <kunalkishore1807@gmail.com>
2023-01-21 14:40:39 +0530

    charts added

---
+++ src/scenes/geography/index.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import GeographyChart from "../../components/GeographyChart";
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
+const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+      <Header title="Geograpgy Chart" subtitle="Simple Geography Chart." />
+
+      <Box height="75vh" border={`1px solid ${colors.grey[100]}`} width='78vw'>
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
+};
+export default Geography;
