30fa625b24daae5c85e116849ff06e1d51758c0d
Kunal Kishore <kunalkishore1807@gmail.com>
2023-01-23 15:45:29 +0530

    line cahrt modified

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@mui/material";
 import { ResponsiveLine } from "@nivo/line";
 import { tokens } from "../theme";
-import { mockLineData as data } from "../data/mockData";
+import { mockLineData as data} from "../data/mockData";
 
 const LineChart = ({isDashboard = false}) => {
   const theme = useTheme();
